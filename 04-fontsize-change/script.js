f2096dea9c030d6463ac63d62f7d7a19b90adfbb
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2021-05-24 11:20:03 -0300

    refactoring some js code from project 4

--- 04-fontsize-change/script.js
+++ 04-fontsize-change/script.js
@@ -1,15 +1,14 @@
-const buttons = document.querySelector('.buttons');
-const btns = buttons.querySelectorAll('.btn');
+const buttons = document.querySelectorAll('.btn');
 const content = document.getElementById('content');
 
-btns.forEach((button, index) => {
+buttons.forEach((button, index) => {
   button.addEventListener('click', () => {
     let current = document.getElementsByClassName('active');
     current[0].className = current[0].className.replace("active", "");
     button.className += " active";
 
-    if (index === 0) content.style.fontSize = '0.5em';
+    if (index === 0) content.style.fontSize = '0.75em';
     else if (index === 1) content.style.fontSize = '1em';
-    else if (index === 2) content.style.fontSize = '1.5em';
+    else if (index === 2) content.style.fontSize = '1.25em';
   })
 });
