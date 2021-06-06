9c85e44dd5d73d313b8935701e9eade165a2cfe6
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2021-06-06 19:27:12 -0300

    17th day project - incrementing counter

---
+++ 17-incrementing-counter/script.js
@@ -0,0 +1,18 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach((counter) => {
+  counter.innerText = '0';
+  const updateCounter = () => {
+    const target = +counter.getAttribute('data-target');
+    const content = +counter.innerText;
+
+    const increment = target / 200;
+    if (content < target) {
+      counter.innerText = `${Math.ceil(content + increment)}`;
+      setTimeout(updateCounter, 1);
+    } else {
+      counter.innerText = target;
+    }
+  }
+  updateCounter();
+});
