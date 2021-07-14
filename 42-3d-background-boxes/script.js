9ed8ae8b64d4fa6844310849b1454e51b33f6fbc
Vinnicius de Oliveira Campagnuci <vinniciuscampagnuci@gmail.com>
2021-07-14 20:30:41 -0300

    42nd project - 3d background boxes

---
+++ 42-3d-background-boxes/script.js
@@ -0,0 +1,17 @@
+const container = document.getElementById('boxes')
+const button = document.getElementById('btn')
+
+button.addEventListener('click', () => container.classList.toggle('big'))
+
+function createBoxes() {
+  for (let i = 0; i < 4; i++) {
+    for (let j = 0; j < 4; j++) {
+      const box = document.createElement('div')
+      box.classList.add('box')
+      box.style.backgroundPosition = `${-j * 125}px ${-i * 125}px`
+      container.appendChild(box)
+    }
+  }
+}
+
+createBoxes()
